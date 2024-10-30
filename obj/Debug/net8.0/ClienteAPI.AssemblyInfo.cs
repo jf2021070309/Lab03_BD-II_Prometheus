c0f2fbc94d6441603f00f380dd1b246dde52c709
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2024-10-30 15:04:39 -0500

    correccion buildimage

--- obj/Debug/net8.0/ClienteAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClienteAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3794a08d68fb824707c10607e0d737adfa29b9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
